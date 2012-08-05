2e70595530c0381b9cbf06e23829faf29d941a0f
Marek Sklodowski <masklod@gmail.com>
2012-08-05 03:52:31 +0200

    Kings

--- Checkers/Piece.cs
+++ Checkers/Piece.cs
@@ -1,4 +1,9 @@
+using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
 
 namespace Checkers
@@ -8,6 +13,7 @@ namespace Checkers
         public Ellipse Drawable { get; set; }
         public bool IsBlack { get; set; }
         public bool IsKing { get; set; }
+        public Image KingImage { get; set; }
         public Field Field { get; set; }
         public Field OldField { get; set; }
         public int AnimStep { get; set; }
@@ -32,6 +38,16 @@ namespace Checkers
                 Drawable.Margin = new Thickness((double)x + Board.PieceMargin, (double)y + Board.PieceMargin, 0, 0);
             else
                 Drawable.Margin = new Thickness(field.DisplayX + Board.PieceMargin, field.DisplayY + Board.PieceMargin, 0, 0);
+
+            if (!IsKing && field.Y == FinishY)
+            {
+                IsKing = true;
+                KingImage = new Image { Source = new BitmapImage(new Uri("ms-appx:///king.png")), Width = Drawable.Width, Height = Drawable.Height, Margin = Drawable.Margin };
+                Board.BoardCanvas.Children.Add(KingImage);
+            }
+
+            if (IsKing)
+                KingImage.Margin = Drawable.Margin;
         }
 
         public void Destroy()
